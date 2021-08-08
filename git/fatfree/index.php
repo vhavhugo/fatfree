aa93c3412258c0d4bb9d793454f31e03d68aa658
vhavhugo <vhavhugo@hotmail.com>
2021-08-08 08:17:08 -0300

    Estrutura básica fatefree do git

---
+++ git/fatfree/index.php
@@ -0,0 +1,91 @@
+<?php
+// composer autoloader for required packages and dependencies
+require_once('vendor/autoload.php');
+
+/** @var \Base $f3 */
+$f3 = \Base::instance();
+
+$f3->set('DEBUG',1);
+if ((float)PCRE_VERSION<8.0)
+	trigger_error('PCRE version is out of date');
+
+// Load configuration
+$f3->config('config.ini');
+
+$f3->route('GET /',
+	function($f3) {
+		$classes=array(
+			'Base'=>
+				array(
+					'hash',
+					'json',
+					'session',
+					'mbstring'
+				),
+			'Cache'=>
+				array(
+					'apc',
+					'apcu',
+					'memcache',
+					'memcached',
+					'redis',
+					'wincache',
+					'xcache'
+				),
+			'DB\SQL'=>
+				array(
+					'pdo',
+					'pdo_dblib',
+					'pdo_mssql',
+					'pdo_mysql',
+					'pdo_odbc',
+					'pdo_pgsql',
+					'pdo_sqlite',
+					'pdo_sqlsrv'
+				),
+			'DB\Jig'=>
+				array('json'),
+			'DB\Mongo'=>
+				array(
+					'json',
+					'mongo'
+				),
+			'Auth'=>
+				array('ldap','pdo'),
+			'Bcrypt'=>
+				array(
+					'openssl'
+				),
+			'Image'=>
+				array('gd'),
+			'Lexicon'=>
+				array('iconv'),
+			'SMTP'=>
+				array('openssl'),
+			'Web'=>
+				array('curl','openssl','simplexml'),
+			'Web\Geo'=>
+				array('geoip','json'),
+			'Web\OpenID'=>
+				array('json','simplexml'),
+			'Web\OAuth2'=>
+				array('json'),
+			'Web\Pingback'=>
+				array('dom','xmlrpc'),
+			'CLI\WS'=>
+				array('pcntl')
+		);
+		$f3->set('classes',$classes);
+		$f3->set('content','welcome.htm');
+		echo View::instance()->render('layout.htm');
+	}
+);
+
+$f3->route('GET /userref',
+	function($f3) {
+		$f3->set('content','userref.htm');
+		echo View::instance()->render('layout.htm');
+	}
+);
+
+$f3->run();
